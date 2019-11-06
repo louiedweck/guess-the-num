e1f152c0e64f40c648116588c72faa596a9a86a7
louiedweck <dweck.louis@gmail.com>
2019-11-05 22:50:25 -0800

    added test file

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+import random
+
+from app import guess_the_number
+
+
+class test_funtions(unittest.TestCase):
+
+    def test_guess_the_number(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

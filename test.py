9636a19ac62e7f69396398beb2ee8b8443d64d50
louiedweck <dweck.louis@gmail.com>
2019-11-06 22:09:40 -0800

    added test

--- test.py
+++ test.py
@@ -7,7 +7,9 @@ from app import guess_the_number
 class test_funtions(unittest.TestCase):
 
     def test_guess_the_number(self):
-        pass
+        guess = 60
+        computer_guess = 60
+        self.assertEqual(guess, computer_guess)
 
 
 if __name__ == '__main__':
